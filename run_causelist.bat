f0f5d9a364c7562867acc0f28462d98f39df8e43
GallupPakistan <github@gallup.com.pk>
2026-08-12 16:35:14 +0500

    Delete run_causelist.bat

--- run_causelist.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-timeout /t 10 /nobreak > nul
-cd /d "C:\Users\Hafiz Ahmed\Desktop\Sindh\Sindh"
-call .venv\Scripts\activate.bat
-echo C:\Users\Hafiz Ahmed\Desktop\Sindh\Sindh\cause_lists | python sindh_causelist_auto.py
